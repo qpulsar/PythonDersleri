cdd7d20b53467de7b89a098960bb147c0dfc0afc
emin <ekorkusuz@hotmail.com>
2019-10-18 18:06:50 +0300

    ilk hafta

---
+++ Bote2019Guz/04_darlan.py
@@ -0,0 +1,15 @@
+#############################
+#***************************#
+#        DARLAN v1.0        #
+# Yazan: Kendinden Menkul   #
+# Lisans : GPL v2           #
+#***************************#
+#############################
+
+r = int(input("Yarı çapı giriniz:")) #girilen ifadeyi tam sayıya çevir
+alan = 3.14 * r *r
+
+print("yarı çapı", r ,"olan dairenin alanı", alan)
+print(f"yarı çapı {r} olan dairenin alanı {alan}")
+print("yarı çapı {} olan dairenin alanı {}".format(r, alan))
+
