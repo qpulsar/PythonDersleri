993565e9c15b8800ffd1105f87adeaa821ee15fa
emin <ekorkusuz@hotmail.com>
2019-11-28 14:00:27 +0300

    sonraki hafta

--- Bote2019Guz/53_random.py
+++ Bote2019Guz/53_random.py
@@ -1,6 +1,11 @@
 import random
 
 dizi = []
-for i in range(0,10):
-    random.randrange(1,13)
+for i in range(0, 10):
+    sayi = random.randrange(1, 10)
+    while sayi in dizi:
+        print("aynısı var", sayi)
+        sayi = random.randrange(1, 11)
+    dizi.append(sayi)
 
+print(dizi)
