9640482219b69d4e4f91e78681b951e87f2271f6
Mehmet Emin Korkusuz <ekorkusuz@gmail.com>
2020-11-23 14:59:05 +0300

    60

--- Bote2020Guz/061_sort.py
+++ Bote2020Guz/061_sort.py
@@ -1,3 +1,5 @@
 """
-1-100 arası 10 adet sayıyı rastgele attıktan sonra sıralayan programı yazınız
+1-100 arası 10 adet sayıyı rastgele attıktan sonra döngü kullanarak sıralayan fonksiyon yazınız
+-bubble
 """
+
